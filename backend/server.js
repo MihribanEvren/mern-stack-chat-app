c6fdade0bdac0a69b249d0e9da8a86ab57145130
Mihriban Evren <mihribanevrenn@gmail.com>
2024-10-15 14:13:50 +0300

    first part of the backend is completed

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import { connectDB } from './config/db.js';
+
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import userRoutes from './routes/user.route.js';
+
+const app = express();
+app.use(cookieParser()); // To parse incoming requests with cookies (req.cookies)
+app.use(express.json()); // To parse incoming requests with JSON payloads (req.body)
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
